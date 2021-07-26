b57d45d3ba90f9045a26263f4615cb85ae2a997e
mikhirurg <mikhail.a.ushakov@gmail.com>
2021-07-26 13:42:33 +0200

    Some fixes

--- build.gradle.kts
+++ build.gradle.kts
@@ -39,6 +39,9 @@ dependencies {
 java {
     sourceCompatibility = JavaVersion.VERSION_11
     targetCompatibility = JavaVersion.VERSION_11
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(11))
+    }
 }
 
 // Configure gradle-intellij-plugin plugin.
