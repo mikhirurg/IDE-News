824fdfe1d1e8ec8b835929bed8353271f04684b0
Mikhail Ushakov <mikhail.a.ushakov@gmail.com>
2025-11-03 16:11:56 +0100

    Gradle update

--- build.gradle.kts
+++ build.gradle.kts
@@ -112,7 +112,7 @@ intellijPlatform {
 
     pluginVerification {
         ides {
-            recommended()
+            create(providers.gradleProperty("platformType"), providers.gradleProperty("platformVersion"))
         }
     }
 }
